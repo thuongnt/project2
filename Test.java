6295655bbf4e8c676dba39befb1e4fafec2d0469
KhiemNS <khiemns.k54@gmail.com>
2014-11-20 00:41:02 +0700

    add two files test.java and test2.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test{
+	public static void main(String[] ags){
+		for(int i=1;i<=100;i++){
+			if(i%3==0){
+				if(i%5==0){
+					continue;
+				} 
+				System.out.println(i);			} 
+	
+		}
+	}
+}
